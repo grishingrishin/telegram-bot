519b1a6aace9dc7c9a5bd345480eff9acf599a09
Егор <ge@devspark.ru>
2021-04-08 16:49:11 +0300

    feat: Added logger for some cb

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,13 @@
 require('dotenv').config();
 import { Telegraf } from 'telegraf';
 import mongoose from 'mongoose';
+import logger from './utils/logger';
 
 mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', err => logger.debug(`Failed to connect to DB ${err}`));
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
@@ -15,7 +16,7 @@ db.once('open', () => {
     bot.launch();
 });
 
-mongoose.connection.on('disconnected', () => console.log('Mongoose connection to DB disconnected'));
+mongoose.connection.on('disconnected', () => logger.debug('Mongoose connection to DB disconnected'));
 
 // Enable graceful stop
 process.on('SIGINT', () => gracefulExit).on('SIGTERM', () => gracefulExit);
@@ -23,7 +24,7 @@ process.on('SIGINT', () => gracefulExit).on('SIGTERM', () => gracefulExit);
 function gracefulExit() {
     mongoose.connection.close(() => {
         bot.stop('SIGTERM');
-        console.log('Mongoose connection with DB is disconnected through app termination');
+        logger.debug('Mongoose connection with DB is disconnected through app termination');
         process.exit(1);
     });
 }
