579775251ffd9c4b06f8dfb1004e2b79a4744c32
Егор <ge@devspark.ru>
2021-04-08 17:21:07 +0300

    feat: Added port value in the .env file and added a new command for development build

--- src/bot.ts
+++ src/bot.ts
@@ -3,16 +3,16 @@ import { Telegraf } from 'telegraf';
 import mongoose from 'mongoose';
 import logger from './utils/logger';
 
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(`mongodb://localhost:${process.env.PORT}/`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
-db.on('error', err => logger.debug(`Failed to connect to DB ${err}`));
+db.on('error', err => logger.error(`Failed to connect to DB ${err}`));
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
 db.once('open', () => {
-    bot.start(ctx => ctx.reply('Welcome'));
+    bot.start(ctx => ctx.reply('Goodbye world'));
     bot.launch();
 });
 
