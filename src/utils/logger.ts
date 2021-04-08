ab66eb6e8fe996d742821ac1c31ccca1311ab9d0
Егор <ge@devspark.ru>
2021-04-08 16:41:03 +0300

    feat: Created logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,21 @@
+import { transports, format, createLogger } from 'winston';
+
+const { combine, timestamp, printf } = format;
+
+const logFormat = printf(({ timestamp, level, message }) => `[${timestamp}] [${level}]${message}`);
+
+const logger = createLogger({
+    transports: [
+        new transports.Console({
+            level: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
+        }),
+        new transports.File({ filename: 'debug.log', level: 'debug' }),
+    ],
+    format: combine(timestamp(), format.splat(), format.simple(), logFormat),
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.debug('Logging initialized at debug level');
+}
+
+export default logger;
