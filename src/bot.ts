a3c634cb0ed78439d12f21ef5a06afe6d0dfbc11
Егор <ge@devspark.ru>
2021-04-12 15:15:28 +0300

    refactoring

--- src/bot.ts
+++ src/bot.ts
@@ -1,29 +1,28 @@
 require('dotenv').config();
-import { Telegraf } from 'telegraf';
-import mongoose from 'mongoose';
-import logger from './utils/logger';
-
-mongoose.connect(`mongodb://localhost:${process.env.PORT}/`, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-
-db.on('error', err => logger.error(`Failed to connect to DB ${err}`));
-db.on('disconnected', () => logger.debug('Mongoose connection to DB disconnected'));
+import { Telegraf, Context } from 'telegraf';
+import fetch from 'node-fetch';
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
 
-db.once('open', () => {
-    bot.start(ctx => ctx.reply('Goodbye world'));
-    bot.launch();
+// prettier-ignore
+bot.start(ctx => ctx.reply('Hello i am Giphy bot. I am use api Giphy for delivery some gifs for you. Use eng keyboard for input some text'));
+
+bot.on('message', ctx => {
+    const message: any = ctx.message;
+    const value: string = message.text.toLowerCase();
+
+    const apiPath = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&rating=g&lang=en&q=${value}`;
+
+    fetch(apiPath)
+        .then(res => res.json())
+        .then(json => {
+            const { data } = json;
+            const randomFile = data[Math.floor(Math.random() * data.length)];
+            ctx.replyWithDocument(randomFile.images.original.mp4);
+        });
 });
 
-// Enable graceful stop
-process.on('SIGINT', () => gracefulExit).on('SIGTERM', () => gracefulExit);
+bot.launch();
 
-function gracefulExit() {
-    mongoose.connection.close(() => {
-        bot.stop('SIGTERM');
-        logger.debug('Mongoose connection with DB is disconnected through app termination');
-        process.exit(1);
-    });
-}
+// Enable graceful stop
+process.on('SIGINT', () => bot.stop('SIGINT')).on('SIGTERM', () => bot.stop('SIGTERM'));
